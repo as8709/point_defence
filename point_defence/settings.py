efabce968f9c508ea39ffef79801a4b50d4faf36
ais <as8709@gmail.com>
2016-09-06 21:42:15 +0100

    Try a different approach to getting static files to work

--- point_defence/settings.py
+++ point_defence/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'bootstrap3',
     'starter',
 ]
@@ -117,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
